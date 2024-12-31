83401306a3d9b37b3caf66bb8ccd182c57585aeb
Avijit Manna <avijit.manna@isubsoft.com>
2024-12-31 12:14:19 +0530

    Convert vcard metadata into conf json file

--- src/Vobject/Reader.php
+++ src/Vobject/Reader.php
@@ -9,152 +9,30 @@ class Reader extends \Sabre\VObject\Reader{
      *
      * @var array
      */
-    private static $VCard_attr_info = [
 
-        'FN' => ['multi_allowed' => false,
-                    'composite_attr'=> false,
-                    'parameter' => []],
-        
-        'N' => ['multi_allowed' => false,
-                    'composite_attr'=> [ 0 => 'last_name', 1 => 'first_name', 2 => 'middle_name', 3 => 'prefix', 4 => 'suffix' ],
-                    'parameter' => []],
+    private static function vCardMetaData(){
 
-        'EMAIL' => ['multi_allowed' => true,
-                        'composite_attr'=> false,
-                        'parameter' => []],
+        $json = file_get_contents(__DIR__.'/../../conf/vcard_metadata.json'); 
 
-        'ORG' => ['multi_allowed' => true,
-                    'composite_attr'=> [ 0 => 'org_name', 1 => 'org_unit_name'],
-                    'parameter' => []],
+        if ($json === false) {
+            return null;
+        }
 
-        'TITLE' => ['multi_allowed' => true,
-                        'composite_attr'=> false,
-                        'parameter' => []],
+        $json_data = json_decode($json, true); 
 
-        'NICKNAME' => ['multi_allowed' => false,
-                            'composite_attr'=> false,
-                            'parameter' => []],
+        if ($json_data === null) {
+            return null;
+        }
 
-        'PHOTO' => ['multi_allowed' => true,
-                        'composite_attr'=> false,
-                        'parameter' => ['TYPE', 'VALUE', 'MEDIATYPE', 'ENCODING']],
-
-        'NOTE' => ['multi_allowed' => true,
-                        'composite_attr'=> false,
-                        'parameter' => []],
-
-        'TEL' => ['multi_allowed' => true,
-                        'composite_attr'=> false,
-                        'parameter' => ['TYPE', 'VALUE']],
-
-        'ADR' => ['multi_allowed' => true,
-                        'composite_attr'=> [ 0 => 'po_box', 1 => 'house_no', 2 => 'street', 3 => 'locality', 4 => 'province', 5 => 'postal_code', 6 => 'country'],
-                        'parameter' => ['TYPE', 'VALUE']],
-
-        'BDAY' => ['multi_allowed' => false,
-                        'composite_attr'=> false,
-                        'parameter' => []],
-
-        'ANNIVERSARY' => ['multi_allowed' => false,
-                                'composite_attr'=> false,
-                                'parameter' => []],
-
-        'GENDER' => ['multi_allowed' => false,
-                            'composite_attr'=> [ 0 => 'sex', '1' => 'gender_identity'],
-                            'parameter' => []],
-
-        'IMPP' => ['multi_allowed' => true,
-                        'composite_attr'=> false,
-                        'parameter' => ['TYPE', 'VALUE']],
-
-        'LANG' => ['multi_allowed' => true,
-                        'composite_attr'=> false,
-                        'parameter' => ['TYPE', 'VALUE']],
-
-        'TZ' => ['multi_allowed' => true,
-                        'composite_attr'=> false,
-                        'parameter' => ['VALUE']],
-
-        'GEO' => ['multi_allowed' => true,
-                        'composite_attr'=> false,
-                        'parameter' => []],
-        
-        'ROLE' => ['multi_allowed' => true,
-                        'composite_attr'=> false,
-                        'parameter' => []],
-
-        'LOGO' => ['multi_allowed' => true,
-                        'composite_attr'=> false,
-                        'parameter' => []],
-
-        'MEMBER' => ['multi_allowed' => true,
-                        'composite_attr'=> false,
-                        'parameter' => []],
-
-        'RELATED' => ['multi_allowed' => true,
-                        'composite_attr'=> false,
-                        'parameter' => ['VALUE']],        
-
-        'BIRTHPLACE' => ['multi_allowed' => false,
-                            'composite_attr'=> false,
-                            'parameter' => ['VALUE', 'LANGUAGE']],
-
-        'DEATHPLACE' => ['multi_allowed' => false,
-                            'composite_attr'=> false,
-                            'parameter' => ['VALUE', 'LANGUAGE']],
-
-        'DEATHDATE' => ['multi_allowed' => false,
-                            'composite_attr'=> false,
-                            'parameter' => ['VALUE', 'CALSCALE', 'LANGUAGE']],
-
-        'EXPERTISE' => ['multi_allowed' => true,
-                        'composite_attr'=> false,
-                        'parameter' => ['LEVEL', 'INDEX']],
-
-        'HOBBY' => ['multi_allowed' => true,
-                        'composite_attr'=> false,
-                        'parameter' => ['LEVEL', 'INDEX']],
-
-        'INTEREST' => ['multi_allowed' => true,
-                            'composite_attr'=> false,
-                            'parameter' => ['LEVEL', 'INDEX']],
-
-        'ORG-DIRECTORY' => ['multi_allowed' => true,
-                                'composite_attr'=> false,
-                                'parameter' => ['PREF', 'INDEX']],
-
-        'CONTACT-URI' => ['multi_allowed' => true,
-                                'composite_attr'=> false,
-                                'parameter' => []],
-
-        'GRAMGENDER' => ['multi_allowed' => true,
-                                'composite_attr'=> false,
-                                'parameter' => ['LANGUAGE']],
-
-        'LANGUAGE' => ['multi_allowed' => false,
-                                'composite_attr'=> false,
-                                'parameter' => []],
-
-        'PRONOUNS' => ['multi_allowed' => true,
-                                'composite_attr'=> false,
-                                'parameter' => ['LANGUAGE', 'PREF', 'TYPE', 'ALTID']],
-
-        'SOCIALPROFILE' => ['multi_allowed' => true,
-                                'composite_attr'=> false,
-                                'parameter' => ['SERVICE-TYPE', 'VALUE']],
-
-        'JSPROP' => ['multi_allowed' => true,
-                                'composite_attr'=> false,
-                                'parameter' => ['JSPTR']]
-                    
-          
-    ];
+        return $json_data;
+    }
 
     function multiAllowedStatus($vCard_attr){    
 
-        if(isset(self::$VCard_attr_info[$vCard_attr]))
+        $VCard_attr_info = self::vCardMetaData();
+        if(isset($VCard_attr_info[$vCard_attr]))
         {
-            return (['status' => self::$VCard_attr_info[$vCard_attr]['multi_allowed']]);
+            return (['status' => $VCard_attr_info[$vCard_attr]['multi_allowed']]);
         }
 
         return false;
@@ -162,9 +40,10 @@ class Reader extends \Sabre\VObject\Reader{
 
     function compositeAttrStatus($vCard_attr){
 
-        if(isset(self::$VCard_attr_info[$vCard_attr]))
+        $VCard_attr_info = self::vCardMetaData();
+        if(isset($VCard_attr_info[$vCard_attr]))
         {
-            return (['status' => self::$VCard_attr_info[$vCard_attr]['composite_attr']]);
+            return (['status' => $VCard_attr_info[$vCard_attr]['composite_attr']]);
         }
 
         return false;
@@ -172,9 +51,10 @@ class Reader extends \Sabre\VObject\Reader{
 
     function parameterStatus($vCard_attr){        
 
-        if(isset(self::$VCard_attr_info[$vCard_attr]))
+        $VCard_attr_info = self::vCardMetaData();
+        if(isset($VCard_attr_info[$vCard_attr]))
         {
-            return (['parameter' => self::$VCard_attr_info[$vCard_attr]['parameter']]);
+            return (['parameter' => $VCard_attr_info[$vCard_attr]['parameter']]);
         }
 
         return false;
