92b97988c2de219ef384883f03ad911bf053745a
Avijit Manna <avijit.manna@isubsoft.com>
2025-04-22 17:41:05 +0530

    Mysql trigger added

--- sql/mysql/ddl.sql
+++ sql/mysql/ddl.sql
@@ -59,3 +59,38 @@ CREATE TABLE cards_full_sync
 	CONSTRAINT cards_full_sync_fk01 FOREIGN KEY(user_id) REFERENCES cards_user (user_id) ON DELETE CASCADE ON UPDATE CASCADE,
 	CONSTRAINT cards_full_sync_fk02 FOREIGN KEY(addressbook_id) REFERENCES cards_addressbook (addressbook_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+
+/**************** Triggers ******************/
+
+DROP TRIGGER IF EXISTS cards_addressbook_before;
+DELIMITER //
+CREATE TRIGGER cards_addressbook_before BEFORE INSERT ON cards_addressbook FOR EACH ROW 
+BEGIN
+	if (NOT NEW.user_specific AND NOT EXISTS (SELECT 1 FROM cards_user WHERE user_id = '__SYS_USER') AND NOT EXISTS (SELECT 1 FROM cards_system_user)) THEN 
+             INSERT INTO cards_user (user_id) VALUES ('__SYS_USER');
+			INSERT INTO cards_system_user (user_id) VALUES ('__SYS_USER');
+	END IF;
+END //
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS cards_backend_map_before;
+DELIMITER //
+CREATE TRIGGER cards_backend_map_before BEFORE INSERT ON cards_backend_map FOR EACH ROW
+BEGIN
+	if (EXISTS (SELECT 1 FROM cards_addressbook WHERE addressbook_id = NEW.addressbook_id AND user_specific) AND NOT EXISTS (SELECT 1 FROM cards_user WHERE user_id = NEW.user_id)) THEN 
+             INSERT INTO cards_user (user_id) VALUES (NEW.user_id);
+	END IF;
+END //
+
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS cards_full_sync_before;
+DELIMITER //
+CREATE TRIGGER cards_full_sync_before BEFORE INSERT ON cards_full_sync FOR EACH ROW
+BEGIN
+	if (EXISTS (SELECT 1 FROM cards_addressbook WHERE addressbook_id = NEW.addressbook_id AND user_specific) AND NOT EXISTS (SELECT 1 FROM cards_user WHERE user_id = NEW.user_id)) THEN 
+             INSERT INTO cards_user (user_id) VALUES (NEW.user_id);
+	END IF;
+END //
+DELIMITER ;
