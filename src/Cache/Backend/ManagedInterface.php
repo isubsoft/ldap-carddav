b5ed6a2263084d432413553ef77d1ca09729e637
Shubhra Prakash Nandi <shubhraprakash.nandi@isubsoft.com>
2026-03-09 14:55:03 +0530

    Updated - Enhancement

--- src/Cache/Backend/ManagedInterface.php
+++ src/Cache/Backend/ManagedInterface.php
@@ -5,7 +5,22 @@
 
 namespace ISubsoft\Cache\Backend;
 
+/**
+* Application managed cache backend interface.
+*
+* This interface need to added by cache backends which
+* are managed by the application like filesystem cache.
+**/
 interface ManagedInterface
 {
+	/**
+	* Implement this method to evict (delete) stale cache items.
+	*
+	* This can clean up space in cache store.
+	*
+  * @param integer $batchSize
+  *
+  * @return bool
+	**/
 	public function evictStale(int $batchSize = 0);
 }
