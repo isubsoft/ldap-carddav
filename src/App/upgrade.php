71d0e05f46956b600ee4e2f53fcf3efa1291968e
Shubhra Prakash Nandi <shubhraprakash.nandi@isubsoft.com>
2026-06-23 20:30:38 +0530

    Updated - Altering required indexes as well.

--- src/App/upgrade.php
+++ src/App/upgrade.php
@@ -56,6 +56,7 @@ $upgradeDbStatements = [
 	'mysql' => [ 
 		"ALTER TABLE cards_addressbook MODIFY COLUMN user_specific CHAR(1) NOT NULL DEFAULT '1', MODIFY COLUMN writable CHAR(1) NOT NULL DEFAULT '1'",
 		"ALTER TABLE propertystorage MODIFY COLUMN path TEXT NOT NULL, MODIFY COLUMN name TEXT NOT NULL, MODIFY COLUMN valuetype INTEGER",
+		"ALTER TABLE propertystorage DROP INDEX path_property, ADD UNIQUE INDEX path_property (path(600), name(100))",
 		mysqlFileGetContents(__BASE_DIR__ . "/sql/mysql/30_trigger_ddl.sql"),
 	],
 	'pgsql' => [
