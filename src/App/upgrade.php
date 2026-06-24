42070d5159cf3500e396890bb44a08b19967aa60
Shubhra Prakash Nandi <shubhraprakash.nandi@isubsoft.com>
2026-06-24 11:02:19 +0530

    Updated

--- src/App/upgrade.php
+++ src/App/upgrade.php
@@ -27,15 +27,15 @@
 require_once __DIR__ . '/include/bootstrap.php';
 
 $syncDbVersion = [
-	'major' => 2,
-	'minor' => 0,
-	'dot'   => 0
+	'major'    => 2,
+	'minor'    => 0,
+	'revision' => 0
 ];
 
 $upgradeCompatibleSyncDbVersion = [
-	'major' => null,
-	'minor' => null,
-	'dot'   => null
+	'major'    => null,
+	'minor'    => null,
+	'revision' => null
 ];
 
 function mysqlFileGetContents($filename)
@@ -52,19 +52,27 @@ function mysqlFileGetContents($filename)
 	return $fileContents;
 }
 
+$createDbVersionTableStmt = "CREATE TABLE schema_version (key_name VARCHAR(32) NOT NULL, value INTEGER NOT NULL DEFAULT 0)";
+
+foreach($syncDbVersion as $key_name => $key_value)
+	$updateDbVersionStmt[] = "INSERT INTO schema_version (key_name, key_value) VALUES ('" . $key_name . "', " . $key_value . ")";
+
 $upgradeDbStatements = [
-	'mysql' => [ 
+	'mysql' => [
+		$createDbVersionTableStmt,
 		"ALTER TABLE cards_addressbook MODIFY COLUMN user_specific CHAR(1) NOT NULL DEFAULT '1', MODIFY COLUMN writable CHAR(1) NOT NULL DEFAULT '1'",
 		"ALTER TABLE propertystorage MODIFY COLUMN path TEXT NOT NULL, MODIFY COLUMN name TEXT NOT NULL, MODIFY COLUMN valuetype INTEGER",
 		"ALTER TABLE propertystorage DROP INDEX path_property, ADD UNIQUE INDEX path_property (path(600), name(100))",
 		mysqlFileGetContents(__BASE_DIR__ . "/sql/mysql/30_trigger_ddl.sql"),
 	],
 	'pgsql' => [
+		$createDbVersionTableStmt,
 		"ALTER TABLE cards_addressbook ALTER COLUMN user_specific TYPE CHAR(1) USING (user_specific::INTEGER), ALTER COLUMN user_specific SET DEFAULT '1', ALTER COLUMN writable TYPE CHAR(1) USING (writable::INTEGER), ALTER COLUMN writable SET DEFAULT '1'",
 		"ALTER TABLE propertystorage ALTER COLUMN path TYPE TEXT, ALTER COLUMN name TYPE TEXT, ALTER COLUMN valuetype TYPE INTEGER",
 		file_get_contents(__BASE_DIR__ . "/sql/pgsql/30_trigger_ddl.sql"),
 	],
 	'sqlite' => [
+		$createDbVersionTableStmt,
 		"ALTER TABLE cards_addressbook RENAME COLUMN user_specific TO old_user_specific",
 		"ALTER TABLE cards_addressbook RENAME COLUMN writable TO old_writable",
 		"ALTER TABLE cards_addressbook ADD COLUMN user_specific CHAR(1) NOT NULL DEFAULT '1'",
@@ -92,6 +100,9 @@ if (!isset($upgradeDbStatements[$pdoDriverName]) || !is_array($upgradeDbStatemen
 
 try {
 	foreach ($upgradeDbStatements[$pdoDriverName] as $stmt)
+		$pdo->exec($stmt);
+		
+	foreach ($updateDbVersionStmt as $stmt)
 		$pdo->exec($stmt);
 } 
 catch (\Throwable $th) {
