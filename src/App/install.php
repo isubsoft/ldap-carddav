bb246e8ed46b688d8e6517a01ba2cce62318ba57
Shubhra Prakash Nandi <shubhraprakash.nandi@isubsoft.com>
2026-06-24 16:04:45 +0530

    Added/updated - Added install script to install sync database schema, mysql create trigger DDL file modified to be used with the new install script.

---
+++ src/App/install.php
@@ -0,0 +1,50 @@
+<?php 
+
+/***************************************************************************
+*
+* Copyright (C) 2023-2025 ISub Softwares (OPC) Private Limited
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*
+***************************************************************************/
+
+/**
+* This script is used to upgrade sync database.
+**/
+
+/*import database connection*/
+require_once __DIR__ . '/include/bootstrap.php';
+
+$installDbStmts = [];
+
+foreach(SUPPOTED_SYNCDB_PRODUCTS as $dbProductName) {
+	foreach(glob(__BASE_DIR__ . "/sql/" . $dbProductName . "/*_ddl.sql", GLOB_ERR) as $ddlSqlFile)
+		$installDbStmts[$dbProductName][] = file_get_contents($ddlSqlFile);
+}
+
+if (!isset($installDbStmts[$pdo_scheme]) || !is_array($installDbStmts[$pdo_scheme])) {
+	echo "[INFO] No install steps defined for '$pdo_scheme' database product.";
+	exit(1);
+}
+
+try {
+	foreach ($installDbStmts[$pdo_scheme] as $stmt)
+		$pdo->exec($stmt);
+} 
+catch (\Throwable $th) {
+	trigger_error("Caught exception. Error message: " . $th->getMessage(), E_USER_WARNING);
+	exit(1);
+}
+
+exit;
