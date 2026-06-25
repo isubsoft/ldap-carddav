e859118a8091ef9c50f2c17547e6c061ad68dfcf
Shubhra Prakash Nandi <shubhraprakash.nandi@isubsoft.com>
2026-06-25 14:01:26 +0530

    Updated/added - Backported relevant changes from upstream commit - b9b64a84f3d21884e7dde4bdbbbff2c96832dd7d.

---
+++ src/App/upgrade.php
@@ -0,0 +1,99 @@
+<?php 
+
+/***************************************************************************
+*
+* Copyright (C) 2023-2025 ISub Softwares (OPC) Private Limited
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*
+***************************************************************************/
+
+/**
+* This script is used to upgrade sync database.
+**/
+
+/*import database connection*/
+require_once __DIR__ . '/include/bootstrap.php';
+
+$syncDbVersion = [
+	'major'    => 2,
+	'minor'    => 0,
+	'revision' => 0
+];
+
+$upgradeCompatibleSyncDbVersion = [
+	'major'    => null,
+	'minor'    => null,
+	'revision' => null
+];
+
+$createDbVersionTableStmt = "CREATE TABLE schema_version (key_name VARCHAR(32) NOT NULL, key_value INTEGER NOT NULL DEFAULT 0)";
+$updateDbVersionStmt = [];
+
+foreach($syncDbVersion as $key_name => $key_value)
+	$updateDbVersionStmt[] = "INSERT INTO schema_version (key_name, key_value) VALUES ('" . $key_name . "', " . $key_value . ")";
+	
+$upgradeDbStatements = [];
+
+if($pdo_scheme == 'mysql') {
+	$upgradeDbStatements = [
+		$createDbVersionTableStmt,
+		"ALTER TABLE cards_addressbook MODIFY COLUMN user_specific CHAR(1) NOT NULL DEFAULT '1', MODIFY COLUMN writable CHAR(1) NOT NULL DEFAULT '1'",
+		"ALTER TABLE propertystorage MODIFY COLUMN path TEXT NOT NULL, MODIFY COLUMN name TEXT NOT NULL, MODIFY COLUMN valuetype INTEGER",
+		"ALTER TABLE propertystorage DROP INDEX path_property, ADD UNIQUE INDEX path_property (path(600), name(100))",
+		file_get_contents(__BASE_DIR__ . "/sql/" . $pdo_scheme . "/30_trigger_ddl.sql")
+	];
+}
+elseif($pdo_scheme == 'pgsql') {
+	$upgradeDbStatements = [
+		$createDbVersionTableStmt,
+		"ALTER TABLE cards_addressbook ALTER COLUMN user_specific TYPE CHAR(1) USING (user_specific::INTEGER), ALTER COLUMN user_specific SET DEFAULT '1', ALTER COLUMN writable TYPE CHAR(1) USING (writable::INTEGER), ALTER COLUMN writable SET DEFAULT '1'",
+		"ALTER TABLE propertystorage ALTER COLUMN path TYPE TEXT, ALTER COLUMN name TYPE TEXT, ALTER COLUMN valuetype TYPE INTEGER",
+		file_get_contents(__BASE_DIR__ . "/sql/" . $pdo_scheme . "/30_trigger_ddl.sql")
+	];
+}
+elseif($pdo_scheme == 'sqlite') {
+	$upgradeDbStatements = [
+		$createDbVersionTableStmt,
+		"ALTER TABLE cards_addressbook RENAME COLUMN user_specific TO old_user_specific",
+		"ALTER TABLE cards_addressbook RENAME COLUMN writable TO old_writable",
+		"ALTER TABLE cards_addressbook ADD COLUMN user_specific CHAR(1) NOT NULL DEFAULT '1'",
+		"ALTER TABLE cards_addressbook ADD COLUMN writable CHAR(1) NOT NULL DEFAULT '1'",
+		"UPDATE cards_addressbook SET user_specific = old_user_specific, writable = old_writable",
+		"ALTER TABLE propertystorage RENAME COLUMN valuetype TO old_valuetype",
+		"ALTER TABLE propertystorage RENAME COLUMN value TO old_value",
+		"ALTER TABLE propertystorage ADD COLUMN valuetype INTEGER",
+		"ALTER TABLE propertystorage ADD COLUMN value BLOB",
+		"UPDATE propertystorage SET valuetype = old_valuetype, value = old_value",
+		file_get_contents(__BASE_DIR__ . "/sql/" . $pdo_scheme . "/30_trigger_ddl.sql"),
+		"ALTER TABLE cards_addressbook DROP COLUMN old_user_specific",
+		"ALTER TABLE cards_addressbook DROP COLUMN old_writable",
+		"ALTER TABLE propertystorage DROP COLUMN old_valuetype",
+		"ALTER TABLE propertystorage DROP COLUMN old_value"
+	];
+}
+
+try {
+	foreach ($upgradeDbStatements as $stmt)
+		$pdo->exec($stmt);
+		
+	foreach ($updateDbVersionStmt as $stmt)
+		$pdo->exec($stmt);
+} 
+catch (\Throwable $th) {
+	trigger_error("Caught exception. Error message: " . $th->getMessage(), E_USER_WARNING);
+	exit(1);
+}
+
+exit;
